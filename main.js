086514a87192ae1376977c1964f31269febe8bb4
Gustav Sobel <gustav@Gustavs-Sobels-iMac.local>
2016-07-02 16:11:35 +0200

    Add autocompletion

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World from js!');
+var app = angular.module("MethodApp", ['ngMaterial']);
+
+app.controller("MethodController", function($scope) {
+    this.querySearch = function (text) {
+      if (!text) {
+        return methods;
+      } else {
+        return methods.filter(function (method) {
+          console.log(method, text);
+          return method.name.match(new RegExp(text, 'gi'));
+        });
+      }
+    };
+});
---
+++ methods.js
@@ -0,0 +1,22 @@
+var methods = [
+  {
+    component: 'ACE (serum)',
+    alternativeName: ['Angiotensin converting enzyme'],
+    system: 'S-',
+    tubeColor: 'gelrör',
+    sample: '1 gelrör (gul propp).',
+    comment: 'Prover från patienter som behandlas med ACE hämmare (t. ex. Captopril) bör analyseras/frysas inom 4 timmar.',
+    referral: {
+      text: 'Klinisk Kemi Special',
+      url: '',
+      notes: []
+    },
+    toLabWithin: '',
+    handling: 'Centrifugera och avskilj serum. Serum fryses inom 4h.',
+    storage: 'I frys. Skickas som frysprov. I undantagsfall kan serum som förvarats i kyl skickas (hållbart ca 1 v i kyl). Frys då provet och skicka det fryst (ange om möjligt hur länge det har kylförvarats).',
+    destination: 'Karolinska(S)',
+    source: 'http://www.karolinska.se/KUL/Alla-anvisningar/Anvisning/9000',
+    updated: '20160721',
+    updatedBy: 'GS'
+  }
+];
