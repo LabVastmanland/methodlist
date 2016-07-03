dc7e48e3b12bd7945ced1280bdc0f71a254acc77
Daniel Ström <D@nielstrom.se>
2016-07-03 15:46:02 +0200

    Add search button and radiobuttons

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var app = angular.module("MethodApp", ['ngMaterial']);
 
 app.controller("MethodController", function($scope) {
-    this.select = function (method) {
-      this.selectedItem = method;
+  $scope.personelType = 'health';
 
+  this.select = function (method) {
+    this.selectedItem = method;
+  }
+
+  this.querySearch = function (text) {
+    if (!text) {
+      return methods;
+    } else {
+      return methods.filter(function (method) {
+        console.log(method, text);
+        return method.name.match(new RegExp(text, 'gi'));
+      });
     }
-    this.querySearch = function (text) {
-      if (!text) {
-        return methods;
-      } else {
-        return methods.filter(function (method) {
-          console.log(method, text);
-          return method.name.match(new RegExp(text, 'gi'));
-        });
-      }
-    };
+  };
 });
