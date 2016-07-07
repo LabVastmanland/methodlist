4277ff32bd5b3f75a99bb468ce5273b90d87fec9
Daniel Ström <D@nielstrom.se>
2016-07-07 19:51:44 +0200

    Make subheader global; Update color depending on your personelType

--- app.js
+++ app.js
@@ -9,4 +9,38 @@ angular
   .config(function($locationProvider, $routeProvider) {
     $routeProvider.otherwise({redirectTo: '/'});
     // $locationProvider.html5Mode(true);
+  })
+  .controller('AppController', function($scope, $q, $location, methods) {
+    $scope.personelType = 'health';
+
+    $scope.methods = methods;
+
+    //TODO: use select instead of location.
+    $scope.$watch('selectedItem', function(method) {
+      if (method) {
+        $scope.select(method.ID);
+      } else {
+        $location.path('/');
+      }
+    });
+
+    $scope.select = function (index) {
+      $location.path('/' + index);
+    }
+
+    $scope.search = function (text) {
+      return $q(function(resolve, reject){
+        if (!text) {
+          resolve($scope.methods);
+        } else {
+          resolve($scope.methods.filter(function (method) {
+            try {
+              return method.Komponent.toLowerCase().indexOf(text.toLowerCase()) != -1;
+            } catch (e) {
+              return false;
+            }
+          }));
+        }
+      });
+    };
   });
--- list/controller.js
+++ list/controller.js
@@ -10,34 +10,4 @@ angular
     });
   })
 
-  .controller("ListController", function($scope, $q, $location, methods) {
-    console.log(methods);
-    $scope.personelType = 'health';
-    $scope.methods = methods;
-
-    $scope.$watch('selectedItem', function(method) {
-      if (method) {
-        $scope.select(method.ID);
-      }
-    })
-
-    $scope.select = function (index) {
-      $location.path('/' + index);
-    }
-
-    $scope.search = function (text) {
-      return $q(function(resolve, reject){
-        if (!text) {
-          resolve($scope.methods);
-        } else {
-          resolve($scope.methods.filter(function (method) {
-            try {
-              return method.Komponent.toLowerCase().indexOf(text.toLowerCase()) != -1;
-            } catch (e) {
-              return false;
-            }
-          }));
-        }
-      });
-    };
-  });
+  .controller("ListController", function() {});
