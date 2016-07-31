2c0569e1e5dd52621549cdcf45b3f856aa4fa9f3
Daniel Ström <D@nielstrom.se>
2016-07-31 21:50:05 +0200

    Use the methodlist from github instead

--- methods.js
+++ methods.js
@@ -2,8 +2,7 @@ angular
   .module("app.methods", ['app.toolbox'])
   .factory('methods', function($http, _) {
     function id(x) { return x; }
-    // return $http.get('https://gustavsobel.github.io/methodlist/data/Analyslista.csv')
-    return $http.get('http://127.0.0.1:8888/data/Analyslista.csv')
+    return $http.get('https://gustavsobel.github.io/methodlist/data/Analyslista.csv')
       .then(function (response) {
         if (response.status !== 200) { throw new Error('Could not fetch method data.') }
 
