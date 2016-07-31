62bc62602187727e907b28153767f8671ad5d54f
Daniel Ström <D@nielstrom.se>
2016-07-31 21:48:55 +0200

    Use an external CSV-parser

--- methods.js
+++ methods.js
@@ -6,26 +6,15 @@ angular
     return $http.get('http://127.0.0.1:8888/data/Analyslista.csv')
       .then(function (response) {
         if (response.status !== 200) { throw new Error('Could not fetch method data.') }
-        var rows = response.data.split('\n'),
-            headers = _.head(rows).split(';'),
-            data = _.tail(rows).map(function (row) {
-              return row.split(';');
-            });
 
-        var objects = data.map(function (values) {
-          if (values.length < headers.length) { return {} };
-
-          var obj = {};
-
-          for (var i = 0; i < headers.length; i++) {
-            obj[headers[i]] = values[i].trim();
-          }
-
-          return obj;
+        var parsed = Papa.parse(response.data, {
+          delimiter: ';',
+          header: true,
+          skipEmptyLines: true
         });
 
-        return objects.filter(function (obj) {
-          return Object.keys(obj).length > 0;
+        return parsed.data.filter(function (obj) {
+          return obj.Komponent;
         });
       });
   });
