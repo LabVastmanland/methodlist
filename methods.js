c6773fd2f7e4b3fd129f9bf4ba62dc90ea7cd23c
Daniel Ström <D@nielstrom.se>
2016-09-08 18:04:55 +0200

    Read directly from xlsx rather than csv

--- methods.js
+++ methods.js
@@ -2,18 +2,22 @@ angular
   .module("app.methods", [])
   .factory('methods', function($http) {
     function id(x) { return x; }
-    return $http.get('https://gustavsobel.github.io/methodlist/data/Analyslista.csv')
+    return $http.get('https://gustavsobel.github.io/methodlist/data/Analyslista.xlsx', { responseType: "arraybuffer" })
       .then(function (response) {
         if (response.status !== 200) { throw new Error('Could not fetch method data.') }
 
-        var parsed = Papa.parse(response.data, {
-          delimiter: ';',
-          header: true,
-          skipEmptyLines: true
-        });
+        /* convert data to binary string */
+        var data = new Uint8Array(response.data);
+        var arr = new Array();
+        for(var i = 0; i != data.length; ++i) {
+          arr[i] = String.fromCharCode(data[i]);
+        }
+        var bstr = arr.join("");
 
-        return parsed.data.filter(function (obj) {
-          return obj.Komponent;
-        });
+        /* Read binary into Workbook object */
+        var wb = XLSX.read(bstr, {type:"binary"});
+
+        // Convert first sheet to json array
+        return XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
       });
   });
