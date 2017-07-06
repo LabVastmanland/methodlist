a6580f31e0c78c276102047e33b91484262f93eb
2dts <per.bjellerup@ltv.se>
2017-07-06 07:58:50 +0200

    Delete notice.js

--- data/notice.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module("app.notice", [])
-  .directive('lmvlNotice', function() {
-    return {
-        template: '<h1>Under utveckling</h1> <p> Detta är en sida under utveckling. Befintlig information är korrekt men uppdateras löpande med fler provtagningsanvisningar. Om en analys inte finns här, hänvisar vi till <a target="_blank" href="https://www.regionvastmanland.se/vardgivare/behandlingsstod/laboratoriemedicin/provtagningsanvisningar/">Laboratoriemedicin Västmanlands nuvarande provtagningssida</a>.</p>'
-      };
-  });
-
-
