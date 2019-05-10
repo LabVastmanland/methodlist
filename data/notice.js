aab3c245edfef998df54315906c9caf178942921
2dts <per.bjellerup@ltv.se>
2019-05-10 08:13:10 +0200

    Update notice.js

--- data/notice.js
+++ data/notice.js
@@ -2,7 +2,7 @@ angular
   .module("app.notice", [])
   .directive('lmvlNotice', function() {
     return {
-        template: '<h1>Våra provtagningsanvisningar har flyttat:</h1> <p><a target="_blank" href="https://regionvastmanland.se/vardgivare/behandlingsstod/laboratoriemedicin/provtagning/">hit</a>.<br/>Vid frågor, kommentarer eller förbättringsförslag, vänligen kontakta laboratoriemedicin Västmanland.</p>'
+        template: '<h1>Våra provtagningsanvisningar har flyttat:</h1> <p><br><a target="_blank" href="https://regionvastmanland.se/vardgivare/behandlingsstod/laboratoriemedicin/provtagning/">Följ denna länk för att komma till vår nya site.</a>.<br/><br>Vid frågor, kommentarer eller förbättringsförslag, vänligen kontakta laboratoriemedicin Västmanland.</p>'
       };
   });
 
